1b423d98dcaada4928dd393dbd6a86d633f0f0df
jpothanc <70871841+jpothanc@users.noreply.github.com>
2024-02-12 11:35:26 +0800

    update the dependencies

--- restq/http_cached_request.py
+++ restq/http_cached_request.py
@@ -1,5 +1,3 @@
-import tempfile
-
 from restq.http_request import HttpRequest
 from restq.disk_cache import DiskCache
 
--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='omnijp',
-    version='1.1',
+    version='1.3',
     packages=find_packages(),
     install_requires=[
-        # Add any dependencies here
+        'retry>=0.9.2',
+        'requests>=2.31.0'
     ],
     entry_points={
         'console_scripts': [
