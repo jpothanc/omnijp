97c79efeaba617cbaf6c8622bd26c4e37e1e413f
jpothanc <70871841+jpothanc@users.noreply.github.com>
2024-02-12 11:57:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='omnijp',
-    version='2.0',
+    version='2.0.0',
     packages=find_packages(),
     install_requires=[
         'retry>=0.9.2',
