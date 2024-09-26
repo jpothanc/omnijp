ddb9c2557d1f277b75896752bb9e510b7e735798
jpothanc <70871841+jpothanc@users.noreply.github.com>
2024-09-27 01:21:20 +0800

    execute dump tables using threadpool

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
             .set_cache_path(CACHE_DIR)
             .set_cache_name("users_simple_cache")
             .set_connection_string(connection_string)
-            .set_dump_all_tables(False)
+            .set_dump_all_tables(True)
         )).execute("select * from equities")
         print(result)
     except Exception as e:
--- src/dbdisk/db_request_executer.py
+++ src/dbdisk/db_request_executer.py
@@ -1,4 +1,8 @@
+import asyncio
 import concurrent.futures
+import os
+from concurrent.futures import ThreadPoolExecutor
+from datetime import time
 
 from src.dbdisk.db_disk_factory import DbDiskFactory
 from src.dbdisk.database.db_service_factory import DbServiceFactory
@@ -12,10 +16,8 @@ class DbDiskRequestExecutor:
         db_service = DbServiceFactory.create_db_service(self.db_disk_request.db_type, self.db_disk_request.connection_string)
         try:
             if self.db_disk_request.dump_all_tables:
-                print("start dump all tables")
-                with concurrent.futures.ThreadPoolExecutor() as executor:
-                    future = executor.submit(self.dump_all_tables, db_service)
-                    return future.result()
+                print("start dumping all tables")
+                return self.dump_all_tables(db_service)
             else:
                 print("dumping query:", query)
                 header, data = db_service.execute(query)
@@ -28,10 +30,20 @@ class DbDiskRequestExecutor:
         table_query = self.db_disk_request.list_tables_query if self.db_disk_request.list_tables_query else db_service.get_all_tables_query()
         print("get all tables from db:",table_query)
         header, data = db_service.execute(table_query)
-        for table in data:
-            query = f"select * from {table[0]}"
-            print("dumping table:", query)
-            header, data = db_service.execute(query)
-            self.db_disk_request.cache_name = table[0]
-            print("creating db disk cache for table:", table[0])
-            return DbDiskFactory.create_db_disk(self.db_disk_request).save(header, data)
+        # dump_table_tasks = [self.dump_table(table, db_service) for table in data]
+        max_workers = min(5, os.cpu_count() + 4)  # Adjust based on CPU count and workload
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            # return executor.map(lambda x: x, dump_table_tasks)
+            # return executor.map(self.dump_table, data, [db_service] * len(data))
+            return executor.map(lambda x: self.dump_table(x, db_service), data)
+
+
+
+    async def dump_table(self,table, db_service):
+        query = f"select * from {table[0]}"
+        print("dumping table:", query)
+        header, data =  await db_service.execute(query)
+        self.db_disk_request.cache_name = table[0]
+        print("creating db disk cache for table:", table[0])
+        result =  DbDiskFactory.create_db_disk(self.db_disk_request).save(header, data)
+        return result
